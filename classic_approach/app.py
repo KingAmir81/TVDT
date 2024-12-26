cd54010427ff8e81ab76dc442c26a76f45735758
a.gholami <a.h.gholami@ibinesh.ir>
2024-12-26 12:58:17 +0330

    background subtracting

--- classic_approach/app.py
+++ classic_approach/app.py
@@ -1,4 +1,19 @@
 import cv2 as cv
 
 if __name__ == "__main__":
-    print("Hello World")
+    backSub = cv.createBackgroundSubtractorKNN()
+
+    cap = cv.VideoCapture('./src/2103099-hd_1280_720_60fps.mp4')
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv.GaussianBlur(frame,(7,7),5)
+        fgmask = backSub.apply(frame)
+        _ , fgmask = cv.threshold(fgmask,200,255,cv.THRESH_BINARY)
+        cv.imshow("frame",fgmask)
+
+        if cv.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
